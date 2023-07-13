5936d61c51750d5a2c019734177b7fa6eea2a996
khasport <khavn1985@gmail.com>
2023-07-13 11:12:42 +0700

    new test

--- main.tf
+++ main.tf
@@ -6,6 +6,6 @@ resource "aws_instance" "hello" {
   ami           = "ami-0df7a207adb9748c7"
   instance_type = "t2.micro"
   tags = {
-    Name = "Auto trigger with webhook"
+    Name = "Auto trigger"
   }
 }
